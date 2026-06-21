815ced49bb0dccf2434a094402020e1cf1b2904d
pauljsnider <211066188+pauljsnider@users.noreply.github.com>
2026-06-21 17:09:31 -0500

    Preserve lazy parent tools adapter bindings

--- apps/app/src/lib/adapters/legacyParentTools.ts
+++ apps/app/src/lib/adapters/legacyParentTools.ts
@@ -1,59 +1,83 @@
 /* Auto-generated typed adapter boundary for legacy js/ parent-tools imports (#2066).
  * Bindings re-exported as-is so existing js/* test mocks apply via the @legacy alias. */
-import { approveTeamRegistration as legacy_approveTeamRegistration, createFamilyShareToken as legacy_createFamilyShareToken, createParentMembershipRequest as legacy_createParentMembershipRequest, createRegistrationCheckoutSession as legacy_createRegistrationCheckoutSession, extendTeamRegistrationOffer as legacy_extendTeamRegistrationOffer, createTeamMediaFolder as legacy_createTeamMediaFolder, createTeamMediaLink as legacy_createTeamMediaLink, discoverPublicTeams as legacy_discoverPublicTeams, getPlayers as legacy_getPlayers, getTeamRegistrationForm as legacy_getTeamRegistrationForm, getTeam as legacy_getTeam, getTeamMediaFolders as legacy_getTeamMediaFolders, getTeamMediaItems as legacy_getTeamMediaItems, canAccessTeamChat as legacy_canAccessTeamChat, listCertificatesForPlayer as legacy_listCertificatesForPlayer, listFamilyShareTokens as legacy_listFamilyShareTokens, listMyParentMembershipRequests as legacy_listMyParentMembershipRequests, listParentTeamFeeRecipients as legacy_listParentTeamFeeRecipients, listTeamRegistrationForms as legacy_listTeamRegistrationForms, listTeamRegistrationReviews as legacy_listTeamRegistrationReviews, listTeamRegistrationReviewsPage as legacy_listTeamRegistrationReviewsPage, rejectTeamRegistration as legacy_rejectTeamRegistration, revokeFamilyShareToken as legacy_revokeFamilyShareToken, updateFamilyShareTokenCalendars as legacy_updateFamilyShareTokenCalendars, uploadTeamMediaFile as legacy_uploadTeamMediaFile, uploadTeamMediaPhoto as legacy_uploadTeamMediaPhoto, deleteTeamMediaItem as legacy_deleteTeamMediaItem, updateTeamMediaItem as legacy_updateTeamMediaItem, moveTeamMediaItems as legacy_moveTeamMediaItems, setTeamMediaAlbumCover as legacy_setTeamMediaAlbumCover } from '@legacy/db.js';
+import * as legacyDb from '@legacy/db.js';
 import { addPendingFamilyMember as legacy_addPendingFamilyMember, readFamilyMembers as legacy_readFamilyMembers } from '@legacy/family-plan.js';
-import { db as legacy_db, doc as legacy_doc, collection as legacy_collection, getDoc as legacy_getDoc, serverTimestamp as legacy_serverTimestamp, runTransaction as legacy_runTransaction } from '@legacy/firebase.js';
+import * as legacyFirebase from '@legacy/firebase.js';
 import { formatParentFeeAmount as legacy_formatParentFeeAmount, formatParentFeeDueDate as legacy_formatParentFeeDueDate, getParentFeeStatusMeta as legacy_getParentFeeStatusMeta, normalizeParentFeeRecord as legacy_normalizeParentFeeRecord, sortParentFeeRecords as legacy_sortParentFeeRecords } from '@legacy/parent-dashboard-fees.js';
-import { cancelStripeRegistrationCheckout as legacy_cancelStripeRegistrationCheckout, initiateTeamFeeCheckout as legacy_initiateTeamFeeCheckout } from '@legacy/stripe-service.js';
+import * as legacyStripeService from '@legacy/stripe-service.js';
 import { buildPendingRegistrationRecord as legacy_buildPendingRegistrationRecord, calculateRegistrationFeeSnapshot as legacy_calculateRegistrationFeeSnapshot, decideRegistrationPlacement as legacy_decideRegistrationPlacement, getActiveRegistrationOptions as legacy_getActiveRegistrationOptions, getPaymentPlanChoices as legacy_getPaymentPlanChoices, getRegistrationPaymentNotice as legacy_getRegistrationPaymentNotice, hasOnlineRegistrationCheckout as legacy_hasOnlineRegistrationCheckout, normalizeRegistrationForm as legacy_normalizeRegistrationForm, requiresRegistrationOption as legacy_requiresRegistrationOption } from '@legacy/registration-flow.js';
 import { getRegistrationGuardianDrafts as legacy_getRegistrationGuardianDrafts, getRegistrationPlayerDraft as legacy_getRegistrationPlayerDraft, getRegistrationSubmittedData as legacy_getRegistrationSubmittedData, normalizeRegistrationStatus as legacy_normalizeRegistrationStatus } from '@legacy/registration-review.js';
 import { canContributeTeamMedia as legacy_canContributeTeamMedia, canManageTeamMedia as legacy_canManageTeamMedia, canReadTeamMediaAlbum as legacy_canReadTeamMediaAlbum, getTeamMediaItemUrl as legacy_getTeamMediaItemUrl, isSafeTeamMediaUrl as legacy_isSafeTeamMediaUrl, sortByMediaOrder as legacy_sortByMediaOrder } from '@legacy/team-media-utils.js';
 
-export const approveTeamRegistration = legacy_approveTeamRegistration as (...args: any[]) => any;
-export const createFamilyShareToken = legacy_createFamilyShareToken as (...args: any[]) => any;
-export const createParentMembershipRequest = legacy_createParentMembershipRequest as (...args: any[]) => any;
-export const createRegistrationCheckoutSession = legacy_createRegistrationCheckoutSession as (...args: any[]) => any;
-export const extendTeamRegistrationOffer = legacy_extendTeamRegistrationOffer as (...args: any[]) => any;
-export const createTeamMediaFolder = legacy_createTeamMediaFolder as (...args: any[]) => any;
-export const createTeamMediaLink = legacy_createTeamMediaLink as (...args: any[]) => any;
-export const discoverPublicTeams = legacy_discoverPublicTeams as (...args: any[]) => any;
-export const getPlayers = legacy_getPlayers as (...args: any[]) => any;
-export const getTeamRegistrationForm = legacy_getTeamRegistrationForm as (...args: any[]) => any;
-export const getTeam = legacy_getTeam as (...args: any[]) => any;
-export const getTeamMediaFolders = legacy_getTeamMediaFolders as (...args: any[]) => any;
-export const getTeamMediaItems = legacy_getTeamMediaItems as (...args: any[]) => any;
-export const canAccessTeamChat = legacy_canAccessTeamChat as (...args: any[]) => any;
-export const listCertificatesForPlayer = legacy_listCertificatesForPlayer as (...args: any[]) => any;
-export const listFamilyShareTokens = legacy_listFamilyShareTokens as (...args: any[]) => any;
-export const listMyParentMembershipRequests = legacy_listMyParentMembershipRequests as (...args: any[]) => any;
-export const listParentTeamFeeRecipients = legacy_listParentTeamFeeRecipients as (...args: any[]) => any;
-export const listTeamRegistrationForms = legacy_listTeamRegistrationForms as (...args: any[]) => any;
-export const listTeamRegistrationReviews = legacy_listTeamRegistrationReviews as (...args: any[]) => any;
-export const listTeamRegistrationReviewsPage = legacy_listTeamRegistrationReviewsPage as (...args: any[]) => any;
-export const rejectTeamRegistration = legacy_rejectTeamRegistration as (...args: any[]) => any;
-export const revokeFamilyShareToken = legacy_revokeFamilyShareToken as (...args: any[]) => any;
-export const updateFamilyShareTokenCalendars = legacy_updateFamilyShareTokenCalendars as (...args: any[]) => any;
-export const uploadTeamMediaFile = legacy_uploadTeamMediaFile as (...args: any[]) => any;
-export const uploadTeamMediaPhoto = legacy_uploadTeamMediaPhoto as (...args: any[]) => any;
-export const deleteTeamMediaItem = legacy_deleteTeamMediaItem as (...args: any[]) => any;
-export const updateTeamMediaItem = legacy_updateTeamMediaItem as (...args: any[]) => any;
-export const moveTeamMediaItems = legacy_moveTeamMediaItems as (...args: any[]) => any;
-export const setTeamMediaAlbumCover = legacy_setTeamMediaAlbumCover as (...args: any[]) => any;
+function callLegacyDb(name: string, args: any[]) {
+  const fn = (legacyDb as Record<string, any>)[name];
+  if (typeof fn !== 'function') {
+    throw new TypeError(`Legacy db binding ${String(name)} is not available.`);
+  }
+  return fn(...args);
+}
+
+function callLegacyFirebase(name: string, args: any[]) {
+  const fn = (legacyFirebase as Record<string, any>)[name];
+  if (typeof fn !== 'function') {
+    throw new TypeError(`Legacy firebase binding ${String(name)} is not available.`);
+  }
+  return fn(...args);
+}
+
+function callLegacyStripeService(name: string, args: any[]) {
+  const fn = (legacyStripeService as Record<string, any>)[name];
+  if (typeof fn !== 'function') {
+    throw new TypeError(`Legacy stripe-service binding ${String(name)} is not available.`);
+  }
+  return fn(...args);
+}
+
+export const approveTeamRegistration = (...args: any[]) => callLegacyDb('approveTeamRegistration', args);
+export const createFamilyShareToken = (...args: any[]) => callLegacyDb('createFamilyShareToken', args);
+export const createParentMembershipRequest = (...args: any[]) => callLegacyDb('createParentMembershipRequest', args);
+export const createRegistrationCheckoutSession = (...args: any[]) => callLegacyDb('createRegistrationCheckoutSession', args);
+export const extendTeamRegistrationOffer = (...args: any[]) => callLegacyDb('extendTeamRegistrationOffer', args);
+export const createTeamMediaFolder = (...args: any[]) => callLegacyDb('createTeamMediaFolder', args);
+export const createTeamMediaLink = (...args: any[]) => callLegacyDb('createTeamMediaLink', args);
+export const discoverPublicTeams = (...args: any[]) => callLegacyDb('discoverPublicTeams', args);
+export const getPlayers = (...args: any[]) => callLegacyDb('getPlayers', args);
+export const getTeamRegistrationForm = (...args: any[]) => callLegacyDb('getTeamRegistrationForm', args);
+export const getTeam = (...args: any[]) => callLegacyDb('getTeam', args);
+export const getTeamMediaFolders = (...args: any[]) => callLegacyDb('getTeamMediaFolders', args);
+export const getTeamMediaItems = (...args: any[]) => callLegacyDb('getTeamMediaItems', args);
+export const canAccessTeamChat = (...args: any[]) => callLegacyDb('canAccessTeamChat', args);
+export const listCertificatesForPlayer = (...args: any[]) => callLegacyDb('listCertificatesForPlayer', args);
+export const listFamilyShareTokens = (...args: any[]) => callLegacyDb('listFamilyShareTokens', args);
+export const listMyParentMembershipRequests = (...args: any[]) => callLegacyDb('listMyParentMembershipRequests', args);
+export const listParentTeamFeeRecipients = (...args: any[]) => callLegacyDb('listParentTeamFeeRecipients', args);
+export const listTeamRegistrationForms = (...args: any[]) => callLegacyDb('listTeamRegistrationForms', args);
+export const listTeamRegistrationReviews = (...args: any[]) => callLegacyDb('listTeamRegistrationReviews', args);
+export const listTeamRegistrationReviewsPage = (...args: any[]) => callLegacyDb('listTeamRegistrationReviewsPage', args);
+export const rejectTeamRegistration = (...args: any[]) => callLegacyDb('rejectTeamRegistration', args);
+export const revokeFamilyShareToken = (...args: any[]) => callLegacyDb('revokeFamilyShareToken', args);
+export const updateFamilyShareTokenCalendars = (...args: any[]) => callLegacyDb('updateFamilyShareTokenCalendars', args);
+export const uploadTeamMediaFile = (...args: any[]) => callLegacyDb('uploadTeamMediaFile', args);
+export const uploadTeamMediaPhoto = (...args: any[]) => callLegacyDb('uploadTeamMediaPhoto', args);
+export const deleteTeamMediaItem = (...args: any[]) => callLegacyDb('deleteTeamMediaItem', args);
+export const updateTeamMediaItem = (...args: any[]) => callLegacyDb('updateTeamMediaItem', args);
+export const moveTeamMediaItems = (...args: any[]) => callLegacyDb('moveTeamMediaItems', args);
+export const setTeamMediaAlbumCover = (...args: any[]) => callLegacyDb('setTeamMediaAlbumCover', args);
 export const addPendingFamilyMember = legacy_addPendingFamilyMember as (...args: any[]) => any;
 export const readFamilyMembers = legacy_readFamilyMembers as (...args: any[]) => any;
-export const db: unknown = legacy_db;
-export const doc = legacy_doc as (...args: any[]) => any;
-export const collection = legacy_collection as (...args: any[]) => any;
-export const getDoc = legacy_getDoc as (...args: any[]) => any;
-export const serverTimestamp = legacy_serverTimestamp as (...args: any[]) => any;
-export const runTransaction = legacy_runTransaction as (...args: any[]) => any;
+export const db: unknown = legacyFirebase.db;
+export const doc = (...args: any[]) => callLegacyFirebase('doc', args);
+export const collection = (...args: any[]) => callLegacyFirebase('collection', args);
+export const getDoc = (...args: any[]) => callLegacyFirebase('getDoc', args);
+export const serverTimestamp = (...args: any[]) => callLegacyFirebase('serverTimestamp', args);
+export const runTransaction = (...args: any[]) => callLegacyFirebase('runTransaction', args);
 export const formatParentFeeAmount = legacy_formatParentFeeAmount as (...args: any[]) => any;
 export const formatParentFeeDueDate = legacy_formatParentFeeDueDate as (...args: any[]) => any;
 export const getParentFeeStatusMeta = legacy_getParentFeeStatusMeta as (...args: any[]) => any;
 export const normalizeParentFeeRecord = legacy_normalizeParentFeeRecord as (...args: any[]) => any;
 export const sortParentFeeRecords = legacy_sortParentFeeRecords as (...args: any[]) => any;
-export const cancelStripeRegistrationCheckout = legacy_cancelStripeRegistrationCheckout as (...args: any[]) => any;
-export const initiateTeamFeeCheckout = legacy_initiateTeamFeeCheckout as (...args: any[]) => any;
+export const cancelStripeRegistrationCheckout = (...args: any[]) => callLegacyStripeService('cancelStripeRegistrationCheckout', args);
+export const initiateTeamFeeCheckout = (...args: any[]) => callLegacyStripeService('initiateTeamFeeCheckout', args);
 export const buildPendingRegistrationRecord = legacy_buildPendingRegistrationRecord as (...args: any[]) => any;
 export const calculateRegistrationFeeSnapshot = legacy_calculateRegistrationFeeSnapshot as (...args: any[]) => any;
 export const decideRegistrationPlacement = legacy_decideRegistrationPlacement as (...args: any[]) => any;
